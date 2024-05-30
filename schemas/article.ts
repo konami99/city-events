2c50bf158dae2f8359851f1d7a105a435926d619
Richard Chou <konami99@hotmail.com>
2024-05-30 22:31:08 +1000

    added article type

---
+++ schemas/article.ts
@@ -0,0 +1,71 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'article',
+  title: 'Article',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'startDate',
+      title: 'Start Date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'endDate',
+      title: 'End Date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'status',
+      title: 'Status',
+      type: 'string',
+      initialValue: 'draft',
+      options: {
+        list: [
+          { title: 'Draft', value: 'draft' },
+          { title: 'Live', value: 'live' },
+        ],
+        layout: 'radio'
+      }
+    }),
+    defineField({
+      name: 'shortDescription',
+      title: 'Short Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+    },
+  },
+})
