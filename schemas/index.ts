407399899a40149ffc84a0faffa04d7fd4cfc1cb
Richard Chou <konami99@hotmail.com>
2024-06-01 09:30:04 +1000

    removed article

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,7 @@
-import article from './article'
 import blockContent from './blockContent'
 import category from './category'
 import event from './event'
 import program from './program'
 import user from './user'
 
-export const schemaTypes = [event, user, category, program, article, blockContent]
+export const schemaTypes = [event, user, category, program, blockContent]
